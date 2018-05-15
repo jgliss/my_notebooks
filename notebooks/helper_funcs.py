7ab540de15124dd317760fbed8a67d7316b12704
Jonas Gliss <jonasgliss@gmail.com>
2018-05-15 19:54:40 +0200

    Worked on conditional colour formatting of Dataframe

--- notebooks/helper_funcs.py
+++ notebooks/helper_funcs.py
@@ -2,6 +2,63 @@
 from pandas import DataFrame
 import numpy as np
 import xlrd
+import matplotlib.cm as colormaps
+import matplotlib.colors as colors
+    
+def shifted_color_map(vmin, vmax, cmap = None):
+    """Shift center of a diverging colormap to value 0
+    
+    .. note::
+    
+        This method was found `here <http://stackoverflow.com/questions/
+        7404116/defining-the-midpoint-of-a-colormap-in-matplotlib>`_ 
+        (last access: 17/01/2017). Thanks to `Paul H <http://stackoverflow.com/
+        users/1552748/paul-h>`_ who provided it.
+    
+    Function to offset the "center" of a colormap. Useful for
+    data with a negative min and positive max and if you want the
+    middle of the colormap's dynamic range to be at zero level
+    
+    :param vmin: lower end of data value range
+    :param vmax: upper end of data value range
+    :param cmap: colormap (if None, use default cmap: seismic)
+    
+    :return: 
+        - shifted colormap
+        
+    """
+
+    if cmap is None:
+        cmap = colormaps.seismic
+    elif isinstance(cmap, str):
+        cmap = colormaps.get_cmap(cmap)
+
+    midpoint = 1 - abs(vmax)/(abs(vmax) + abs(vmin))
+    cdict = {
+        'red': [],
+        'green': [],
+        'blue': [],
+        'alpha': []
+    }
+
+    # regular index to compute the colors
+    reg_index = np.linspace(0, 1, 257)
+
+    # shifted index to match the data
+    shift_index = np.hstack([
+        np.linspace(0.0, midpoint, 128, endpoint=False), 
+        np.linspace(midpoint, 1.0, 129, endpoint=True)
+    ])
+
+    for ri, si in zip(reg_index, shift_index):
+        r, g, b, a = cmap(ri)
+
+        cdict['red'].append((si, r, r))
+        cdict['green'].append((si, g, g))
+        cdict['blue'].append((si, b, b))
+        cdict['alpha'].append((si, a, a))
+
+    return colors.LinearSegmentedColormap('shiftedcmap', cdict)
 
 def read_var_info_michaels_excel(xlspath):
     """Read short description strings for variables
